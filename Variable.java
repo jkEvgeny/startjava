9c5212facafd60c0dc97bab48e291f5d0484e89c
jkEvgeny <jk.galchenko@yandex.ru>
2021-06-03 00:11:43 +0300

    first commit

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable{
+	public static void main(String[] args){
+		byte b = 1;
+		short s = 5;
+		int i = 8;
+		long l = 1;
+		float f= 1;
+		double d= 1.6;
+		char c = 'i';
+		boolean bool = false;	
+
+		System.out.println("Values: " + b + " " + 
+										s + " " +
+										i + " " +
+										l + " " +
+										f + " " +
+										d + " " +
+										c + " " +
+										bool);
+	}	
+}
