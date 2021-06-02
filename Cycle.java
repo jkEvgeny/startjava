ada79be6a0b92878ce81cf56ae35d6d113bb19cd
jkEvgeny <jk.galchenko@yandex.ru>
2021-06-03 00:46:31 +0300

    third commit

---
+++ Cycle.java
@@ -0,0 +1,26 @@
+public class Cycle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
+
+        int i = 6;
+        int result = -6;
+
+        while (i >= result) {
+            System.out.println(i);
+            i -= 2;
+        }
+
+        int j = 10;
+        result = 20;
+        int sum = 0;
+        do {
+            if (j % 2 != 0) {
+                sum+=j;
+            }
+            j++;
+        } while (j <= result);
+        System.out.println(sum);
+    }
+}
