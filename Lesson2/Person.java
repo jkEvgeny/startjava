c94929c530170908f1f50c8ae6efde05570a65e5
jkEvgeny <jk.galchenko@yandex.ru>
2021-06-04 13:29:24 +0300

    Lesson2 adding Person

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    public String sex = "male";
+    public String name = "James";
+    public String surname = "Bo";
+    public int height = 130;
+    public int weight = 15;
+    public int age = 32;
+
+    void go() {
+        System.out.println("James goes");
+    }
+
+    void sit() {
+        System.out.println("James sits");
+    }
+
+    void run() {
+        System.out.println("James runs");
+    }
+
+    void speak() {
+        System.out.println("James speaks");
+    }
+
+    boolean isLearningJava() {
+        return true;
+    }
+}
