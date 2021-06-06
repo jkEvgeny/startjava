1ab7e41a3203ee93cca9153f54207a5d7d3cb7cd
jkEvgeny <jk.galchenko@yandex.ru>
2021-06-06 15:43:04 +0300

    Lesson2 Review fixes.3

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         Scanner scan = new Scanner(System.in);
         String yesOrNo;
         do {
             System.out.println("Введите первое число: ");
-            int firstNumber = in.nextInt();
+            int firstNumber = scan.nextInt();
 
+            scan.nextLine();
             System.out.println("Введите знак математической операции: ");
             String sign = scan.nextLine();
 
             System.out.println("Введите второе число: ");
-            int secondNumber = in.nextInt();
+            int secondNumber = scan.nextInt();
 
             Calculator.calculate(firstNumber, secondNumber, sign);
             do {
+                scan.nextLine();
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
                 yesOrNo = scan.nextLine();
             } while (!yesOrNo.equals("yes") && !yesOrNo.equals("no"));
